32e1e9b5291e8880e7cc16c8465c6002eecbad97
thinva <34601860+hsytkm@users.noreply.github.com>
2023-12-30 22:46:02 +0900

    up

--- AvaloniaApp/ViewLocator.cs
+++ AvaloniaApp/ViewLocator.cs
@@ -11,10 +11,7 @@ public sealed class ViewLocator : IDataTemplate
         ArgumentNullException.ThrowIfNull(data, nameof(data));
 
         var name = data.GetType().FullName!.Replace("ViewModel", "View");
-
-#pragma warning disable IL2057 // Unrecognized value passed to the parameter of method. It's not possible to guarantee the availability of the target type.
         var type = Type.GetType(name);
-#pragma warning restore IL2057 // Unrecognized value passed to the parameter of method. It's not possible to guarantee the availability of the target type.
 
         if (type is null)
             return new TextBlock { Text = "Not Found: " + name };
